fdeb0ecac660129d69fa2ace8c0847b65b8e659a
Nancy <ny6049812@gmail.com>
2025-08-04 11:13:04 +0300

    P01-Q01

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -7,7 +7,32 @@ namespace OOP04
         static void Main(string[] args)
         {
 
+            #region P01 Q01
+            Calculator calc = new Calculator();
 
+            Console.WriteLine(calc.Add(2, 3));      
+            Console.WriteLine(calc.Add(1, 2, 3));      
+            Console.WriteLine(calc.Add(2.5, 3.7));
+
+            #endregion
+
+
+            #region
+
+            #endregion
+
+
+            #region
+
+            #endregion
+
+            #region
+
+            #endregion
+
+            #region
+
+            #endregion
 
         }
     }
